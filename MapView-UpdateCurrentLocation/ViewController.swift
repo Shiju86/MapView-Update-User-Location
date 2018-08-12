2ea83ca44c393ef3ccfad568469ec5a1d651d2c8
Shiju Varghese <shiju86.v@gmail.com>
2018-08-12 14:24:12 +0530

    Initial Commit

---
+++ MapView-UpdateCurrentLocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapView-UpdateCurrentLocation
+//
+//  Created by Shiju Varghese on 12/08/18.
+//  Copyright © 2018 Shiju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
